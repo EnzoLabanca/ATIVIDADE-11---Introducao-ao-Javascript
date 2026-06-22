2a2ad7235c5a3233ad790e44fd91ac3145186478
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-06-22 16:39:41 -0300

    Create 05.js

---
+++ 05.js
@@ -0,0 +1,5 @@
+let nota1 = Number(prompt("qual a 1 nota do aluno"));
+let nota2 = Number(prompt("qual a 2 nota do aluno"));
+let nota3 = Number(prompt("qual a 3 nota do aluno"));
+let resultado = (nota1 + nota2 + nota3) / 3
+console.log("resultado: " + resultado);

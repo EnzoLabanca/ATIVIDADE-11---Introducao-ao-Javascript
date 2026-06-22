35b623c96943e9b621ae90313767f9c7fe606d4d
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-06-22 16:09:39 -0300

    Create 02.js

---
+++ 02.js
@@ -0,0 +1,4 @@
+let ano_de_nascimento = Number(prompt("insira sua data de nacimento"));
+let ano_atual = Number(prompt("qual a data atual"));
+let resultado = ano_atual - ano_de_nascimento;
+console.log("resultado: " + resultado);

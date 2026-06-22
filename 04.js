07e13c240f8ac0ca905d3c14a0ed100e4859aa6f
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-06-22 16:31:11 -0300

    Create 04.js

---
+++ 04.js
@@ -0,0 +1,4 @@
+let valor_total = Number(prompt("qual o valor da conta"));
+let amigos = Number(prompt("quantas pessoas a conta sera dividida"));
+let resultado = valor_total / amigos;
+console.log("resultado: " + resultado);

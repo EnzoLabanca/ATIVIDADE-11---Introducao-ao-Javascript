2a42f90e560274a23fd703d0d814763977805586
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-06-22 15:55:46 -0300

    Create 01.js

---
+++ 01.js
@@ -0,0 +1,5 @@
+let numero1 = Number(prompt("inisra o 1° numero: "));
+let numero2 = Number(prompt("insira o 2° numero: "));
+const resultado = numero1 + numero2;
+console.log("resultado: " + resultado);
+

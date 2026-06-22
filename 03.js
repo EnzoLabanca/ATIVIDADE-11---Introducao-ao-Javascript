8ad6f1d5dbc4e3d8c61d853e9613d636afc13f47
EnzoLabanca <0001271687@senaimgaluno.com.br>
2026-06-22 16:21:40 -0300

    Create 03.js

---
+++ 03.js
@@ -0,0 +1,4 @@
+let comprimento = Number(prompt("digite o comprimento da sala"));
+let largura = Number(prompt("digite a largura da sala"));
+let resultado = largura * comprimento;
+console.log("resultado: " + resultado);
